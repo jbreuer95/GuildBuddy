452c07e91db6d4bb6f1ab0f0f7964aa2d6d0e8fd
Jelle Breuer <jelle@breuclan.nl>
2020-03-15 15:36:13 +0100

    Added sha hashing lib

---
+++ Libs/LibSHA1-1.0/Create-Lib.lua
@@ -0,0 +1,22 @@
+-- This file is WoW exclusive, so no need for WoW versus non-WoW tests
+
+-- Ensure LibStub is available.
+local MAJOR, MINOR = "LibSHA1", 3
+assert(LibStub, MAJOR .. " requires LibStub")
+
+-- Create and register the library.
+local sha1, oldversion = LibStub:NewLibrary(MAJOR, MINOR)
+if not sha1 then return end
+
+
+-- Set up debug functions and code
+sha1.DEBUGMODE = false
+--@alpha@
+sha1.DEBUGMODE = true
+--@end-alpha@
+
+
+function sha1.DebugPrint(...)
+    if not sha1.DEBUGMODE then return end
+    print (...)
+end -- DebugPrint()
